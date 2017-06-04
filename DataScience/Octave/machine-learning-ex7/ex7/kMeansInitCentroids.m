2d384e7f91c660b3debab3c20c4a6e881dbbd63b
ymlai87416 <ymlai87416@gmail.com>
2017-06-04 19:54:44 +0800

    after andrew ml course

---
+++ DataScience/Octave/machine-learning-ex7/ex7/kMeansInitCentroids.m
@@ -0,0 +1,25 @@
+function centroids = kMeansInitCentroids(X, K)
+%KMEANSINITCENTROIDS This function initializes K centroids that are to be 
+%used in K-Means on the dataset X
+%   centroids = KMEANSINITCENTROIDS(X, K) returns K initial centroids to be
+%   used with the K-Means on the dataset X
+%
+
+% You should return this values correctly
+centroids = zeros(K, size(X, 2));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: You should set centroids to randomly chosen examples from
+%               the dataset X
+%
+
+m = size(X, 1);
+idx = floor(1+(m-1+1)*rand(1, K));
+centroids = X(idx,:);
+
+
+
+% =============================================================
+
+end
+
